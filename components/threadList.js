080f552cdbf5bc9ebf0614ab10050e1beea6613a
Thomas O'Brien <tbobrien612@gmail.com>
2019-04-28 13:01:50 -0400

    Clean up threadList

--- components/threadList.js
+++ components/threadList.js
@@ -1,17 +1,22 @@
-import { Row, Col, Card } from "react-bootstrap";
+import { Row, Col, Card } from 'react-bootstrap'
+import React, { Fragment } from 'react'
 
-export default ({ threads }) => {
+const ThreadsList = ({ threads }) => {
   return (
-    <Row>
-      <Col lg={12}>
-        {Object.keys(threads).map((id, index) => {
-          return (
-            <Card key={id}>
-              <div>{threads[id].name}</div>
-            </Card>
-          );
-        })}
-      </Col>
-    </Row>
-  );
-};
+    <Fragment>
+      <Row>
+        <Col lg={12}>
+          {Object.keys(threads).map((id, index) => {
+            return (
+              <Card key={id}>
+                <div>{threads[id].name}</div>
+              </Card>
+            )
+          })}
+        </Col>
+      </Row>
+    </Fragment>
+  )
+}
+
+export default ThreadsList
