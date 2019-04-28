ba4b60d735b5274d198a16ca2c1707ba09d2284d
Thomas O'Brien <tbobrien612@gmail.com>
2019-04-28 13:02:35 -0400

    Clean up input and index

--- components/input.js
+++ components/input.js
@@ -1,33 +1,33 @@
-import React from "react";
-import { FormGroup, FormControl } from "react-bootstrap";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { changedInput } from "../store/actions";
+import React from 'react'
+import { FormGroup, FormControl } from 'react-bootstrap'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { changedInput } from '../store/actions'
 
 const Input = ({ changedInput, name, disabled, controlLabel, val, type }) => {
   const onChange = async e => {
-    changedInput(name, e.target.value);
-  };
+    changedInput(name, e.target.value)
+  }
   return (
     <div>
       <FormGroup controlId="formBasicText">
         <FormControl
           disabled={disabled}
-          type={type || "Text"}
+          type={type || 'Text'}
           placeholder={controlLabel}
           onChange={onChange}
           value={val}
         />
       </FormGroup>
     </div>
-  );
-};
+  )
+}
 
 Input.propTypes = {
-  changedInput: PropTypes.func.isRequired
-};
+  changedInput: PropTypes.func.isRequired,
+}
 
 export default connect(
   null,
   { changedInput }
-)(Input);
+)(Input)
--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,54 @@
-import { connect } from "react-redux";
-import React, { Fragment, useEffect } from "react";
-import { Col } from "react-bootstrap";
-import { ThreadList } from "../components/";
+import { connect } from 'react-redux'
+import React, { Fragment, useEffect } from 'react'
+import { ThreadList } from '../components/'
 import {
   requestingThreads,
   requestedThreadsSuccess,
-  requestedThreadsError
-} from "../store/actions";
-import { textile } from "../textile";
+  requestedThreadsError,
+} from '../store/actions'
+import { textile } from '../textile'
 
-import "../styles/index.scss";
+import '../styles/index.scss'
 
 const Home = ({
   requestingThreads,
   requestedThreadsSuccess,
   requestedThreadsError,
   profile,
-  threads
+  threads,
 }) => {
   useEffect(() => {
     const loadThreads = async () => {
-      requestingThreads(profile.id);
+      requestingThreads(profile.id)
       try {
-        const threadsList = await textile.threads.list();
-        requestedThreadsSuccess(threadsList);
+        const threadsList = await textile.threads.list()
+        requestedThreadsSuccess(threadsList)
       } catch (error) {
-        requestedThreadsError(error);
+        requestedThreadsError(error)
       }
-    };
+    }
 
-    loadThreads();
-  }, []);
+    loadThreads()
+  }, [])
 
   return (
     <Fragment>
       <h3>Streams</h3>
       <ThreadList threads={threads.items} />
     </Fragment>
-  );
-};
+  )
+}
 
 const mapStateToProps = ({ threads, profile }) => ({
   threads,
-  profile
-});
+  profile,
+})
 
 export default connect(
   mapStateToProps,
   {
     requestingThreads,
     requestedThreadsSuccess,
-    requestedThreadsError
+    requestedThreadsError,
   }
-)(Home);
+)(Home)
