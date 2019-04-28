e79a72f8762b77910cc02437f6a53389a44a0ee3
Thomas O'Brien <tbobrien612@gmail.com>
2019-04-28 12:53:20 -0400

    Add threadList on Home

--- components/threadList.js
+++ components/threadList.js
@@ -1,55 +1,17 @@
-import { connect } from 'react-redux'
-import { useEffect } from 'react'
 import { Row, Col, Card } from 'react-bootstrap'
-import {
-  requestingThreads,
-  requestedThreadsSuccess,
-  requestedThreadsError
-} from '../store/actions'
-import Clock from './clock'
-import { textile } from '../textile'
 
-const ThreadList = ({
-  requestingThreads,
-  requestedThreadsSuccess,
-  requestedThreadsError,
-  profile
-}) => {
-  useEffect(() => {
-    const loadThreads = async () => {
-      requestingThreads(profile.id)
-      try {
-        const threads = await textile.threads.list()
-        requestedThreadsSuccess(threads)
-      } catch (error) {
-        requestedThreadsError(error)
-      }
-    }
-
-    loadThreads()
-  }, [])
-
+export default ({ threads }) => {
   return (
-    <Row className="custom-container">
+    <Row>
       <Col lg={12}>
-        <Card>
-          <Card.Body>list here</Card.Body>
-        </Card>
+        {Object.keys(threads).map((id, index) => {
+          return (
+            <Card key={id}>
+              <div>{threads[id].name}</div>
+            </Card>
+          )
+        })}
       </Col>
     </Row>
   )
 }
-
-const mapStateToProps = ({ threads, profile }) => ({
-  threads,
-  profile
-})
-
-export default connect(
-  mapStateToProps,
-  {
-    requestingThreads,
-    requestedThreadsSuccess,
-    requestedThreadsError
-  }
-)(ThreadList)
--- pages/index.js
+++ pages/index.js
@@ -1,79 +1,55 @@
-import React, { Fragment } from 'react'
+import { connect } from 'react-redux'
+import React, { Fragment, useEffect } from 'react'
 import { Col } from 'react-bootstrap'
-
-import Logo from '../components/Logo'
+import { ThreadList } from '../components/'
+import {
+  requestingThreads,
+  requestedThreadsSuccess,
+  requestedThreadsError
+} from '../store/actions'
+import { textile } from '../textile'
 
 import '../styles/index.scss'
 
-const Home = () => 
-  <Fragment>
-    <Col
-      lg={{ span: 7, order: -1 }}
-      md={12}
-      sm={{ order: 12 }}
-      className="left-panel"
-    >
-      <div>
-        <h5 className="header">Let's work better.</h5>
-        <p className="first-paragraph">
-          {"Right now we're working on a "}
-          <a
-            href="https://github.com/aragon/flock/blob/master/teams/Autark/2019Q1-2.md"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {'grant'}
-          </a>
-          {' from '}
-          <a
-            href="https://aragon.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Aragon
-          </a>
-          {
-            ' to build peer-to-peer storage for their discussion and profile features.'
-          }
-        </p>
-        <div>
-          <p className="second-paragraph">
-            {
-              "We're also having conversations with researchers of existential threats like AI, climate change, poverty, and aging. If this is you, "
-            }
-            <a href="mailto:jpschwartz8990@gmail.com">we'd love to chat</a>.
-            We're trying to understand how we might help you archive or
-            share your work.
-          </p>
-        </div>
-      </div>
-      <div className="footer">
-        <a
-          className="footer-link"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://twitter.com/openworklabs"
-        >
-          twitter
-        </a>
-        <a
-          className="footer-link"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/openworklabs"
-        >
-          github
-        </a>
-      </div>
-    </Col>
-    <Col
-      lg={{ span: 5, order: 12 }}
-      md={{ span: 12, order: 1 }}
-      className="right-panel"
-    >
-      <Logo />
-      <h2>Open Work Labs</h2>
-    </Col>
-  </Fragment>
+const Home = ({
+  requestingThreads,
+  requestedThreadsSuccess,
+  requestedThreadsError,
+  profile,
+  threads
+}) => {
+  useEffect(() => {
+    const loadThreads = async () => {
+      requestingThreads(profile.id)
+      try {
+        const threadsList = await textile.threads.list()
+        requestedThreadsSuccess(threadsList)
+      } catch (error) {
+        requestedThreadsError(error)
+      }
+    }
 
-export default Home
+    loadThreads()
+  }, [])
+
+  return (
+    <Fragment>
+      <h3>Streams</h3>
+      <ThreadList threads={threads.items} />
+    </Fragment>
+  )
+}
+
+const mapStateToProps = ({ threads, profile }) => ({
+  threads,
+  profile
+})
+
+export default connect(
+  mapStateToProps,
+  {
+    requestingThreads,
+    requestedThreadsSuccess,
+    requestedThreadsError
+  }
+)(Home)
